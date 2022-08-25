afa4410ad10fda24169cd717df228072ec713a5f
Jason Cassella <jasoncassella1@gmail.com>
2022-08-25 12:31:00 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const PORT = 8000;
+require('dotenv').config();
+
+let db,
+  dbConnectionString = process.env.DB_STRING,
+  dbName = 'sample_mflix', collection;
+
+MongoClient.connect(dbConnectionString)
+  .then(client => {
+    console.log('Connected to database');
+    db = client.db(dbName);
+    collection = db.collection('movies');
+  });
