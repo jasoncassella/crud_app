ece82f3131f4bc9c14da8fbb61915b0f50c2f6ce
Jason Cassella <jasoncassella1@gmail.com>
2022-08-25 13:40:43 -0400

    testing without local port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 require('dotenv').config();
 
 // variables
